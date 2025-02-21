7360fb27e442304de6d01e7f4b7ff1d93eee3928
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-21 22:34:08 +0000

    Add nvidia images & start build)

---
+++ scripts/update-aurora-nv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+#Update akmods by re-enabling repo temporarily
+dnf5 -y copr enable ublue-os/akmods 
+dnf5 -y update --refresh
+dnf5 -y copr disable ublue-os/akmods
+
+echo 'done!'

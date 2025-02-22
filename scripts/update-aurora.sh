4f6559b308fefd53c7f517b7829398e24333bb0c
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-22 23:18:16 +0000

    Update update-aurora.sh
    Add Done message!

--- scripts/update-aurora.sh
+++ scripts/update-aurora.sh
@@ -18,9 +18,4 @@ dnf5 -y copr enable ublue-os/akmods
 dnf5 -y update --refresh 
 dnf5 -y copr disable ublue-os/akmods
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+echo 'done!'

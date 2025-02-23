82bd0a91459b341bfbd0a37cab04b44ea4ff4f7d
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-23 17:35:43 +0000

    Resolve 'run cancelled' error & hopefully get steam to install

--- scripts/update-aurora-nv.sh
+++ scripts/update-aurora-nv.sh
@@ -9,8 +9,10 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 -y clean all
+# Perform a distro-sync (again)
+dnf5 -y update
+dnf5 -y distro-sync --allowerasing
+dnf5 -y upgrade --skip-broken
 
 #Update akmods by re-enabling repo temporarily
 dnf5 -y copr enable ublue-os/akmods 
@@ -24,8 +26,19 @@ dnf5 -y copr disable danayer/linux-firmware-git
 
 dnf5 -y clean all
 
+
+#Steam Installation
+#Prepare Steam Dependencies
+dnf5 -y install pipewire 
+
+# Perform a distro-sync (update) (again)
+dnf5 -y update
+dnf5 -y distro-sync --allowerasing
+dnf5 -y upgrade --skip-broken
 #Install Steam - taken from fedora copr repo - cannot be used in tj5-os due to way it's built currently
-dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
- 
-dnf5 -y install steam
+dnf5 -y update --refresh
+
+ #Install Steam Package
+sudo dnf install steam --allowerasing -v --skip-broken
---
+++ scripts/update-bazzite.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+
+#Update akmods by re-enabling repo temporarily
+dnf5 -y copr enable ublue-os/akmods 
+dnf5 -y update --refresh 
+dnf5 -y copr disable ublue-os/akmods
+
+#Add the latest linux firmware!
+dnf5 -y copr enable danayer/linux-firmware-git 
+dnf5 -y install linux-firmware 
+dnf5 -y copr disable danayer/linux-firmware-git 
+
+dnf5 -y clean all
+
+
+#Steam Installation
+#Prepare Steam Dependencies
+dnf5 -y install pipewire 

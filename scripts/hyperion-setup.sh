a08679311860e3e34432033c9e26ca418fd94deb
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-04-14 14:17:04 +0100

    Add cachyos Kernel to Hyperion image

--- scripts/hyperion-setup.sh
+++ scripts/hyperion-setup.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo 'Just a Test!' 
+# Perform a distro-sync (again)
+dnf5 -y update
+#dnf5 -y distro-sync --allowerasing
+dnf5 -y upgrade
+
+#Update akmods by re-enabling repo temporarily
+dnf5 -y copr enable ublue-os/akmods 
+dnf5 -y update --refresh 
+dnf5 -y copr disable ublue-os/akmods
+
+#Add the latest linux firmware!
+dnf5 -y copr enable danayer/linux-firmware-git 
+dnf5 -y install linux-firmware 
+dnf5 -y copr disable danayer/linux-firmware-git
+
+# Install terra repository
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
+#Install CachyOS Kernel
+echo 'Installing CachyOS kernel'
+dnf5 -y copr enable bieszczaders/kernel-cachyos
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
+
+echo 'Done!'
+
+dnf5 -y update 
+dnf5 -y clean all

0a6a76b62cef6283825c8131417fd68a999dbca0
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-04-17 11:32:05 +0100

    Use official CachyOS kernel

--- scripts/misc/cachy-kernel.sh
+++ scripts/misc/cachy-kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf5 -y copr enable whitehara/kernel-cachyos-preempt
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:whitehara:kernel-cachyos-preempt kernel
-dnf5 -y copr disable whitehara/kernel-cachyos-preempt
+dnf5 -y copr enable bieszczaders/kernel-cachyos
+sudo rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
+dnf5 -y copr disable bieszczaders/kernel-cachyos

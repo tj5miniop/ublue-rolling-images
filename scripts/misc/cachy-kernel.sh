1fec82ac418a12a509f6b43df021dc589c6f36a8
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-04-17 12:06:15 +0100

    Remove COSMIC builds, start building aurora AMD

--- scripts/misc/cachy-kernel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-sudo rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
-dnf5 -y copr disable bieszczaders/kernel-cachyos
--- scripts/update-aurora-base.sh
+++ scripts/update-aurora-base.sh
@@ -9,16 +9,21 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-#Remove any remnants
-rm -rf /usr/lib/systemd/system/cosmic-greeter.service
+# Perform a distro-sync (again)
+dnf5 -y update
+#dnf5 -y distro-sync --allowerasing
+dnf5 -y upgrade
 
-dnf5 -y autoremove
+# Enable Podman and Distrobox (making sure they're updated)
+dnf5 -y install podman distrobox git
 
-#install COSMIC Desktop environment
-dnf5 -y copr enable ryanabx/cosmic-epoch
-dnf5 -y install cosmic-desktop sddm NetworkManager
-systemctl disable cosmic-greeter
-dnf5 -y copr disable ryanabx/cosmic-epoch
+# install kora icon theme 
+cd /tmp
+git clone https://github.com/bikass/kora.git
+cd kora
+cp -r kora /usr/share/icons
+cd ../ 
+rm -rf kora
 
 #Update akmods by re-enabling repo temporarily
 dnf5 -y copr enable ublue-os/akmods 
@@ -30,31 +35,38 @@ dnf5 -y copr enable danayer/linux-firmware-git
 dnf5 -y install linux-firmware 
 dnf5 -y copr disable danayer/linux-firmware-git
 
+#Re-enable ASUS linux COPR repo
+dnf5 -y copr enable lukenukem/asus-linux
+dnf5 -y update && dnf5 -y upgrade
+dnf5 -y copr enable lukenukem/asus-linux
 dnf5 -y clean all
 
+
+#Remove Firefox
+dnf5 -y remove firefox
+dnf5 -y install dnf-plugins-core
+
+#Steam Installation
 #Prepare Steam Dependencies
-dnf5 -y install pipewire sassc 
- 
-#Install Dependencies for apps such as steam - Enable Multimedia codes
+dnf5 -y install pipewire 
+
+# Perform a distro-sync (update) (again)
+dnf5 -y update
+dnf5 -y upgrade 
+#Install Steam - taken from fedora copr repo - cannot be used in tj5-os due to way it's built currently
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
+dnf5 -y update --refresh
 
+ #Install Steam Package
+dnf5 -y install steam --allowerasing
 
-# enable mesa-git copr repo - NOTE - will be included in NVIDIA Images - 
-dnf5 -y copr enable xxmitsu/mesa-git
-dnf5 -y update 
-dnf5 -y upgrade 
-dnf5 -y copr disable xxmitsu/mesa-git
-
-#Install CachyOS Optimizations 
+#Enable CachyOS optimisations
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
-dnf5 -y install uksmd bore-sysctl cachyos-settings --allowerasing --skip-unavailable
+dnf5 -y update
+dnf5 -y install libcap-ng libcap-ng-devel procps-ng procps-ng-devel
+dnf5 -y install cachyos-settings --allowerasing
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
-
 dnf5 -y clean all
 
 
-#Regenerate Kernel Modules 
-dracut --regenerate-all --force
-
-systemctl enable sddm
